fb194d1a2e56617d907d70194c1d18682fb0f2c9
Mohamad Naufal Azim Bin Mohd Fauzay <B78824@C349786.local>
2024-07-09 16:59:38 +0800

    add button

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Todolist} from '../models/todolist';
 
 @Component({
   selector: 'app-list',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  appointment : string = "First appointment";
+  // appointment : Todolist = {
+  //   id: 1,
+  //   title: 'Eat at Cafe',
+  //   date: new Date('2024-03-21'),
+  // }
+
+   //create new todolist
+   newTodolistTitle : string = '';
+   newTodolistDate : Date = new Date();
+
+  appointments : Todolist[] = []
+
+  //button method:
+  addTodo() {
+    //to check it response:
+    // alert(this.newTodolistTitle+" "+ this.newTodolistDate);
+
+    //check if have any value:
+    if(this.newTodolistTitle.trim().length && this.newTodolistDate) {
+      let newTodolist: Todolist = {
+        id: Date.now(),
+        title: this.newTodolistTitle,
+        date: this.newTodolistDate,
+      }
+      this.appointments.push(newTodolist);
+
+      //when click add -> reset back 
+      this.newTodolistTitle = '';
+      this.newTodolistDate = new Date();
+
+      alert(this.appointments.length); //see how many elements
+
+    }
+
+  }
+
 }
