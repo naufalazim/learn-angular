24cf7820b928ffbe2a99c1a1ab78eef6e61ad134
Mohamad Naufal Azim Bin Mohd Fauzay <B78824@C349786.local>
2024-07-10 10:46:36 +0800

    learn ngOnInit

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Todolist} from '../models/todolist';
+
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
   // appointment : Todolist = {
   //   id: 1,
   //   title: 'Eat at Cafe',
@@ -18,6 +19,13 @@ export class ListComponent {
    newTodolistDate : Date = new Date();
 
   appointments : Todolist[] = []
+
+  //OnInit -> for when we reload the page, the data is still exist (Because the data is exist on local storage but dont appear on webpage)
+  ngOnInit(): void {
+    console.log("Testing load data")
+    let savedItems = localStorage.getItem("appointments")
+    this.appointments = savedItems ? JSON.parse(savedItems) : []
+  }
 
   //button method:
   addTodo() {
@@ -39,12 +47,17 @@ export class ListComponent {
 
       alert(this.appointments.length); //see how many elements
 
+      //put data to session local storage (not DB):
+      localStorage.setItem("appointments", JSON.stringify(this.appointments));
+
     }
   }
 
   //delete method:
   deleteTodolist(index: number){
     this.appointments.splice(index,1);
+  //remove data to session local storage (not DB):
+  localStorage.setItem("appointments", JSON.stringify(this.appointments));
   }
 
 }
